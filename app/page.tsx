c84be1f8ab0cc02a474b364a8c862ed8ce6ea639
leejz <957675793@qq.com>
2024-10-17 23:24:37 +0800

    Search component commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import {Hero} from "@/components";
+import {CustomFilter, SearchBar} from "@/components";
 
 export default function Home() {
     return (
-        <div>
+        <main className="overflow-hidden">
             <Hero/>
-        </div>
+            <div className="mt-12 padding-x padding-y max-width " id="discover">
+                <div className="home__text-container">
+                    <h1 className="text-7xl font-extrabold">Car Catalogue</h1>
+                    <p>Explore the cars you might like</p>
+                </div>
+                <div className="home__filters">
+                    <SearchBar/>
+                    <div className="home__filter-container">
+                        <CustomFilter/>
+                        <CustomFilter/>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
