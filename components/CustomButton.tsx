9963be67074ec76754191a16d04ef9280ea39825
leejz <957675793@qq.com>
2024-10-17 14:02:36 +0800

    customButton components commit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,10 +1,17 @@
+"use client";
 import React from 'react';
-
-const CustomButton = () => {
+import Image from "next/image";
+import {CustomButtonProps} from "@/types";
+const CustomButton = (customButtonProps: CustomButtonProps) => {
     return (
-        <div>
-
-        </div>
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn ${customButtonProps.containerStyles}`}
+            onClick={customButtonProps.handleClick}
+        >
+            <span className={`flex-1`}>{customButtonProps.title}</span>
+        </button>
     );
 };
 
